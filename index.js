dc7f2e66cf8793cc0aaf0d6965c9d6ff69447c47
username <user@email.mail>
2019-08-26 12:37:09 +0900

    a

---
+++ index.js
@@ -0,0 +1,8 @@
+        function myFunc(){
+            var pTag = document.getElementsByTagName('p');
+            console.log(pTag[0].childNodes[0].baseURI);
+        }
+        function myFunc1(){
+            var rag="<ul><li>first</li><li>second</li><ul>";
+            document.getElementById('result').innerHTML = tag;
+        }
